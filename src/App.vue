a48e35d59f51397c5e4960b1f4d722c0a9c05915
“gaojikai” <1602405417@qq.com>
2022-10-16 18:10:40 +0800

    音乐播放器推荐页面开发

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,21 @@
 <template>
   <div id="app">
-    Hello world
+    <m-header></m-header>
+    <tab></tab>
+    <router-view></router-view>
   </div>
 </template>
 
-<script></script>
+<script>
+import MHeader from "components/m-header/m-header";
+import Tab from "components/tab/tab";
+export default {
+  components: {
+    MHeader,
+    Tab,
+  },
+};
+</script>
 
 <style scoped lang="stylus" rel="stylesheet/stylus">
-  @import "~common/stylus/variable";
-#app {
-  color: $color-theme;
-
-}
 </style>
