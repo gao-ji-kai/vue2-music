00f81125141f525fa099491131d4d674b81adc48
“gaojikai” <1602405417@qq.com>
2022-10-16 10:17:59 +0800

    vue2-music项目初始化

---
+++ src/main.js
@@ -0,0 +1,13 @@
+import Vue from 'vue'
+import App from './App'
+
+import 'common/stylus/index.styl'
+// eslint-disable-next-line no-multiple-empty-lines
+
+
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  render: h => h(App)
+})
