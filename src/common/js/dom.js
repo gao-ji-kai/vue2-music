1828fe4c7ee19098031f04bb954b93641348d22a
“gaojikai” <1602405417@qq.com>
2022-10-17 21:57:44 +0800

    轮播图页面完善

---
+++ src/common/js/dom.js
@@ -0,0 +1,14 @@
+//dom操作的代码
+export function addClass(el, className) {//dom对象和class
+  if (hasClass(el, className)) {
+    return
+  }
+  let newClass = el.className.split(' ')
+  newClass.push(className)
+  el.className = newClass.join(' ')
+}
+
+export function hasClass(el, className) {
+  let reg = new RegExp('(^|\\s)' + className + '(\\s|$)')
+  return reg.test(el.className)
+}
